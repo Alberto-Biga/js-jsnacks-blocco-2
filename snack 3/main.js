38640e5c09901028c70acbbd0ad857804236370f
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-18 18:27:55 +0100

    snack3

---
+++ snack 3/main.js
@@ -0,0 +1,6 @@
+//array numeri 
+let numeri = ["1","2","3","4","5","6","7","8","9","10"]
+//ciclo for per stampare numeri dispari (camio incremento di i)
+for( let i = 1; i <= numeri.length; i += 2 ){
+    console.log(i)
+}
